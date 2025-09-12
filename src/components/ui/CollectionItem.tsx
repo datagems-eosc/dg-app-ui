66d3eca9e0bc8df7054ce2753b23ae57e7fd7830
Nadya Marchenko <nadya.marchenko@idego.io>
2025-09-12 16:37:12 +0200

    fix colour of collection item in side bar

--- src/components/ui/CollectionItem.tsx
+++ src/components/ui/CollectionItem.tsx
@@ -66,8 +66,8 @@ export function CollectionItem({
       </div>
       <Link
         href={createUrl(href)}
-        className={`w-full flex-1 flex items-center px-3 py-2 text-body-16-medium rounded-lg transition-colors relative min-w-0 ${
-          isActive ? "bg-blue-75" : "text-gray-700 hover:bg-slate-75"
+        className={`w-full flex-1 flex items-center px-3 py-2 text-body-16-medium rounded-lg transition-colors relative min-w-0 text-gray-750 ${
+          isActive ? "bg-blue-75" : "hover:bg-slate-75"
         }`}
         title={title}
         onClick={handleLinkClick}
