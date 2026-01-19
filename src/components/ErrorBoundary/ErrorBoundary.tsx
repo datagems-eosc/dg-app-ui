cf49ed80acfafbeffb9cd138bb494f6fb7842bb4
Piotr Sikora <piotr.sikora@espeo.pl>
2026-01-19 17:06:15 +0100

    DG-127|build fix

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -57,7 +57,8 @@ export class ErrorBoundary extends Component<Props, State> {
               Something went wrong
             </h1>
             <p className="text-gray-600 mb-6">
-              We're sorry for the inconvenience. Please try refreshing the page.
+              We&apos;re sorry for the inconvenience. Please try refreshing the
+              page.
             </p>
             <button
               onClick={() => window.location.reload()}
